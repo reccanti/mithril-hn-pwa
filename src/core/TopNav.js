242335bc45928204d1af6759c361d3f47d844afa
Ben Wilcox <reccanti@gmail.com>
2017-06-18 01:26:38 -0400

    Add TopNav links
    Adds the TopNav links that will eventually link to different sections of
    the site

--- src/core/TopNav.js
+++ src/core/TopNav.js
@@ -7,7 +7,7 @@ import m from "mithril"
 const TopNav = {
     view: vnode => {
         return (
-            <nav>Top Navigation</nav>
+            <nav>Mithril HN <a href="/new" oncreate={ m.route.link }>new</a> <a href="/comments" oncreate={ m.route.link }>comments</a> <a href="/show" oncreate={ m.route.link }>show</a> <a href="/ask" oncreate={ m.route.link }>ask</a> <a href="/jobs" oncreate={ m.route.link }>jobs</a></nav>
         )
     }
 }
