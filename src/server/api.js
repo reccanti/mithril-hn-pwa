51082cae092f881604e5eb9badf6b3e086edb950
Ben Wilcox <reccanti@gmail.com>
2017-06-23 19:29:27 -0400

    Finish creating stories api method
    Creates a method to retrieve a generic list of stories

--- src/server/api.js
+++ src/server/api.js
@@ -1,5 +1,5 @@
 const firebase = require('firebase');
-
+const url = require('url');
 
 /**
  * An API to access HackerNews information. Inspired by 
@@ -9,6 +9,37 @@ firebase.initializeApp({
   databaseURL: 'https://hacker-news.firebaseio.com',
 });
 var hn = firebase.database().ref('/v0');
+
+/**
+ * A function which shapes the output of HN's response
+ * for a single item
+ */
+function shapeOutput(item) {
+
+    let output = {
+        id: item.id,
+        title: item.title,
+        points: item.score,
+        user: item.by,
+        time: item.time,
+        comments_count: item.descendants || 0,
+    };
+
+    // set the url
+    if (item.url) {
+        output.url = item.url;
+        output.domain = url.parse(item.url).hostname.replace(/^www\./i, ''); // remove www from the url
+    } else {
+        output.url = `item/${item.id}`; // a local link
+    }
+
+    // identify ask stories
+    if (item.type == 'story' && output.url.match(/^item/i) && item.title.match(/^ask/i)){
+        output.type = 'ask';
+    }
+
+    return output;
+}
 
 /**
  * Fetches a list of stories from the API
@@ -22,31 +53,27 @@ function stories(base, options, fn) {
 
     const stories = hn.child(base).limitToFirst(limit * page);
     stories.once('value', snapshot => {
+
+        // get the subsection of itemIDs we want and convert them to promises
         const items = snapshot.val().slice(startIndex, endIndex);
         const itemFetches = items.map(itemID => {
             return new Promise((resolve, reject) => {
                 const item = hn.child(`item/${itemID}`);
                 item.once('value',
-                          snap => resolve(snap.val()),
-                          err => reject(err)
+                    snap => resolve(snap.val()),
+                    err => reject(err)
                 );
             });
         });
+
+        // fetch all items and perform the callback on them
         Promise.all(itemFetches).then(res => {
             const apiRes = res.filter(Boolean).map(item => {
-                const output = {
-                    id: item.id,
-                    title: item.title,
-                    points: item.score,
-                    user: item.by,
-                    time: item.time,
-                    comments_count: item.descendants || 0,
-                };
-                return output;
+                return shapeOutput(item);
             });
-            console.log(apiRes);
-        });
+            fn(null, apiRes);
+        }).catch(err => fn(err));
     });
 }
 
-stories('topstories');
+stories('topstories', {}, (err, res) => console.log(res));
